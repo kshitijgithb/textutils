9a4afcfb93ad7587be82917f98237afac0530d9f
kshitijgithb <kshitijkumargupta2017@gmail.com>
2023-03-11 23:43:41 +0530

    Updates

---
+++ precache-manifest.eda1fb6cce34f9e83befd253c6f3e34a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ac5f6c5f67e598d959d",
+    "url": "/textutils/static/css/main.6903223c.chunk.css"
+  },
+  {
+    "revision": "6ac5f6c5f67e598d959d",
+    "url": "/textutils/static/js/main.6ac5f6c5.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/textutils/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/textutils/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "5a46c5b2b82118354fea",
+    "url": "/textutils/static/js/runtime~main.5a46c5b2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8839ae1b5f9dccac9bc1bf2a87952cd6",
+    "url": "/textutils/index.html"
+  }
+];
